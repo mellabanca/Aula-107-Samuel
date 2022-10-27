7817628bfb550a3242a6f05befc06b6c3ceb556d
mellabanca <91677889+mellabanca@users.noreply.github.com>
2022-10-27 16:18:28 -0300

    27/10/2022

---
+++ object_tracking.py
@@ -0,0 +1,45 @@
+import cv2
+import time
+import math
+
+video = cv2.VideoCapture("bb3.mp4")
+
+tracker = cv2.TrackerCSRT_create()
+
+returned, img = video.read()
+
+bbox = cv2.selectROI("Rastreando", img, False)
+
+tracker.init(img, bbox)
+
+print(bbox)
+
+def drawBox(img, bbox):
+    x, y, w, h = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
+    cv2.rectangle(img, (x,y), ((x+w), (y+h)), (255,0,255), 3, 1)
+    cv2.putText(img, "Rastreandation........", (75,90), cv2.FONT_HERSHEY_DUPLEX, 0.7, (0,255,0), 2)
+
+while True:
+    check,img = video.read() 
+
+    success, bbox = tracker.update(img)  
+
+    if success:
+        drawBox(img, bbox)
+    else:
+        cv2.putText(img, "Errroooooooou", (75,90), cv2.FONT_HERSHEY_DUPLEX, 0.7, (0,0,255), 2)
+
+    cv2.imshow("resultado",img)
+            
+    key = cv2.waitKey(25)
+
+    if key == 32:
+        print("Interrompido!")
+        break
+
+
+video.release()
+cv2.destroyALLwindows()
+
+
+
